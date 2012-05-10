ed47c4d34b85cf047da76de65c89e6c491a5ef49
TrineStudio <godblessedmay@gmail.com>
2012-05-11 01:02:21 +0800

    All UI Completed!
    Signed-off-by: TrineStudio <godblessedmay@gmail.com>

---
+++ Project/Admin/Admin/MainPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Admin
+{
+    public partial class MainPage : Form
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void go_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Services service = new Services();
+            service.Show();
+        }
+
+        private void MainPage_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Project/Admin/Admin/ServiceCardLost.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Admin
+{
+    public partial class ServiceCardLost : Form
+    {
+        public ServiceCardLost()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void charge_Click(object sender, EventArgs e)
+        {
+            ServiceRecharge serviceRechage = new ServiceRecharge();
+            serviceRechage.Show();
+            this.Hide();
+        }
+
+        private void balanceQuery_Click(object sender, EventArgs e)
+        {
+            Services services = new Services();
+            services.Show();
+            this.Hide();
+        }
+
+        private void change_Click(object sender, EventArgs e)
+        {
+            ServiceChange serviceChange = new ServiceChange();
+            serviceChange.Show();
+            this.Hide();
+        }
+
+        private void historyQuery_Click(object sender, EventArgs e)
+        {
+            ServiceHistory serviceHistory = new ServiceHistory();
+            serviceHistory.Show();
+            this.Hide();
+        }
+
+        private void ServiceCardLost_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
