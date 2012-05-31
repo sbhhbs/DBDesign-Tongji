2ea5a59790822a4f6746c3afc2cc485686113fc7
TrineStudio <godblessedmay@gmail.com>
2012-05-31 10:13:03 +0800

    Nothing
    Signed-off-by: TrineStudio <godblessedmay@gmail.com>

--- Project/Transportation/Transportation/MainPage.Designer.cs
+++ Project/Transportation/Transportation/MainPage.Designer.cs
@@ -39,10 +39,9 @@
             this.railSystemButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
             this.railSystemButton.FlatAppearance.BorderSize = 0;
             this.railSystemButton.Image = ((System.Drawing.Image)(resources.GetObject("railSystemButton.Image")));
-            this.railSystemButton.Location = new System.Drawing.Point(277, 200);
-            this.railSystemButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.railSystemButton.Location = new System.Drawing.Point(307, 188);
             this.railSystemButton.Name = "railSystemButton";
-            this.railSystemButton.Size = new System.Drawing.Size(258, 42);
+            this.railSystemButton.Size = new System.Drawing.Size(194, 34);
             this.railSystemButton.TabIndex = 0;
             this.railSystemButton.UseVisualStyleBackColor = false;
             this.railSystemButton.Click += new System.EventHandler(this.railSystemButton_Click);
@@ -53,23 +52,21 @@
             this.transportSystemButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
             this.transportSystemButton.FlatAppearance.BorderSize = 0;
             this.transportSystemButton.Image = ((System.Drawing.Image)(resources.GetObject("transportSystemButton.Image")));
-            this.transportSystemButton.Location = new System.Drawing.Point(277, 271);
-            this.transportSystemButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.transportSystemButton.Location = new System.Drawing.Point(307, 262);
             this.transportSystemButton.Name = "transportSystemButton";
-            this.transportSystemButton.Size = new System.Drawing.Size(258, 42);
+            this.transportSystemButton.Size = new System.Drawing.Size(194, 34);
             this.transportSystemButton.TabIndex = 1;
             this.transportSystemButton.UseVisualStyleBackColor = false;
             this.transportSystemButton.Click += new System.EventHandler(this.transportSystemButton_Click);
             // 
             // MainPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(782, 595);
+            this.ClientSize = new System.Drawing.Size(784, 602);
             this.Controls.Add(this.transportSystemButton);
             this.Controls.Add(this.railSystemButton);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainPage";
             this.Text = "公交系统";
             this.Load += new System.EventHandler(this.Form1_Load);
--- Project/Transportation/Transportation/RailSystem.Designer.cs
+++ Project/Transportation/Transportation/RailSystem.Designer.cs
@@ -41,10 +41,9 @@
             // 
             this.map.Image = ((System.Drawing.Image)(resources.GetObject("map.Image")));
             this.map.InitialImage = null;
-            this.map.Location = new System.Drawing.Point(43, 49);
-            this.map.Margin = new System.Windows.Forms.Padding(4);
+            this.map.Location = new System.Drawing.Point(41, 47);
             this.map.Name = "map";
-            this.map.Size = new System.Drawing.Size(507, 508);
+            this.map.Size = new System.Drawing.Size(512, 505);
             this.map.TabIndex = 0;
             this.map.TabStop = false;
             this.map.Click += new System.EventHandler(this.map_Click);
@@ -55,19 +54,18 @@
             // button_OK
             // 
             this.button_OK.Image = ((System.Drawing.Image)(resources.GetObject("button_OK.Image")));
-            this.button_OK.Location = new System.Drawing.Point(682, 299);
-            this.button_OK.Margin = new System.Windows.Forms.Padding(4);
+            this.button_OK.Location = new System.Drawing.Point(679, 283);
             this.button_OK.Name = "button_OK";
-            this.button_OK.Size = new System.Drawing.Size(73, 29);
+            this.button_OK.Size = new System.Drawing.Size(79, 29);
             this.button_OK.TabIndex = 1;
             this.button_OK.UseVisualStyleBackColor = true;
             // 
             // button_Return
             // 
-            this.button_Return.Location = new System.Drawing.Point(682, 350);
-            this.button_Return.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button_Return.Location = new System.Drawing.Point(679, 330);
+            this.button_Return.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button_Return.Name = "button_Return";
-            this.button_Return.Size = new System.Drawing.Size(73, 29);
+            this.button_Return.Size = new System.Drawing.Size(79, 29);
             this.button_Return.TabIndex = 2;
             this.button_Return.Text = "返回";
             this.button_Return.UseVisualStyleBackColor = true;
@@ -78,10 +76,9 @@
             this.startPos.BackColor = System.Drawing.Color.Transparent;
             this.startPos.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.startPos.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.startPos.Location = new System.Drawing.Point(664, 149);
-            this.startPos.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.startPos.Location = new System.Drawing.Point(664, 151);
             this.startPos.Name = "startPos";
-            this.startPos.Size = new System.Drawing.Size(37, 27);
+            this.startPos.Size = new System.Drawing.Size(30, 22);
             this.startPos.TabIndex = 3;
             this.startPos.Text = "ad";
             // 
@@ -92,25 +89,23 @@
             this.endPos.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.endPos.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.endPos.Location = new System.Drawing.Point(664, 239);
-            this.endPos.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.endPos.Name = "endPos";
-            this.endPos.Size = new System.Drawing.Size(37, 27);
+            this.endPos.Size = new System.Drawing.Size(30, 22);
             this.endPos.TabIndex = 4;
             this.endPos.Text = "ad";
             this.endPos.Click += new System.EventHandler(this.label1_Click);
             // 
             // RailSystem
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(782, 595);
+            this.ClientSize = new System.Drawing.Size(784, 602);
             this.Controls.Add(this.endPos);
             this.Controls.Add(this.startPos);
             this.Controls.Add(this.button_Return);
             this.Controls.Add(this.button_OK);
             this.Controls.Add(this.map);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "RailSystem";
             this.RightToLeftLayout = true;
             this.Text = "地铁出行查询";
