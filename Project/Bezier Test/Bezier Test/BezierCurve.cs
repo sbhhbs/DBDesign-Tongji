02e05361cd96d2464bdeffcf4e58f65f26d73aba
TrineStudio <godblessedmay@gmail.com>
2012-06-06 14:41:36 +0800

    Add the Bezier Curve Program
    Signed-off-by: TrineStudio <godblessedmay@gmail.com>

---
+++ Project/Bezier Test/Bezier Test/BezierCurve.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Bezier_Test
+{
+    class BezierCurve
+    {
+        List<Bezier> beziers = new List<Bezier>();
+
+        double pos;
+
+        public BezierCurve(List<Bezier> beziers)
+        {
+            pos = 0.0;
+            this.beziers = beziers;
+        }
+
+        public Point move()
+        {
+            int i = (int)pos;
+
+            double count = pos - i;
+
+            if (i > 0 && count == 0)
+            {
+                i--;
+                count = 1;
+            }
+
+            Bezier bezier = beziers.ElementAt(i);
+
+            pos += Bezier.stepLength;
+
+            return bezier.getPoint(count);
+        }
+
+        public Boolean isEnd()
+        {
+            return pos == (double)(beziers.Count + Bezier.stepLength);
+        }
+    }
+}
